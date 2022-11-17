0f84d2a91e3b817bb25a8818cbb3350abf7c21e1
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-11-17 16:48:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    float a, b, x, y, z;
+    cin >>a >>b >>x >>y;
+
+    if ((x<0) && (y>0)){
+    z=(a*x) - (b*y);
+    cout << z << endl;
+
+    }else if ((x>=0) && (y<=0)) {
+    z=(a*x*x) - (b*y);
+    cout << z << endl;
+
+    }else z=(a*x) + (b*y*y);
+    cout << z << endl;
+
+    return 0;
 }
